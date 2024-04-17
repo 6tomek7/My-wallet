86776efeebe3fc63cbecb72924b14ab3008dd27f
Krzysiek <krzysiekwichura@gmail.com>
2024-04-17 06:42:35 +0200

    update changes

--- src/components/FieldToForm.js
+++ src/components/FieldToForm.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./fieldToForm.css";
-const FieldToForm = ({ name, type }) => {
+const FieldToForm = (props) => {
+  const { name, type, errorMessage } = props;
   return (
     <div className="d-flex flex-column mb-2">
       <label htmlFor={name}>{name}</label>
       <input type={type} id={name} />
+      <span className="error">{errorMessage}</span>
     </div>
   );
 };
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -9,16 +9,36 @@ const Registration = () => {
     confirmationPassword: "",
   };
 
-  const handleSubmit = () => {
-    console.log("hello");
+  const handleSubmit = (event) => {
+    console.log(event);
+    event.preventDefault();
   };
 
   const formDescription = [
-    { name: "User name", type: "text" },
-    { name: "Email", type: "email" },
-    { name: "Password", type: "password" },
-    { name: "Confirm Password", type: "password" },
+    {
+      id: 1,
+      name: "User name",
+      type: "text",
+      errorMessage: "User name is too short",
+    },
+    { id: 2, name: "Email", type: "email", errorMessage: "Wrong email adress" },
+    {
+      id: 3,
+      name: "Password",
+      type: "password",
+      errorMessage: "Minimal length for password is 6 characters",
+    },
+    {
+      id: 4,
+      name: "Confirm Password",
+      type: "password",
+      errorMessage: "Password don't match.",
+    },
   ];
+
+  const testFunction = () => {
+    const regex = "[a-z]{3,30}";
+  };
 
   return (
     <div className="container">
@@ -26,11 +46,18 @@ const Registration = () => {
 
       <form onSubmit={handleSubmit} className="p-5 ">
         {formDescription.map((e) => (
-          <FieldToForm name={e.name} type={e.type} />
+          <FieldToForm
+            key={e.id}
+            name={e.name}
+            type={e.type}
+            errorMessage={e.errorMessage}
+          />
         ))}
         <div className="d-flex flex-column align-items-center w-100">
           <br />
-          <button type="submit">Submit</button>
+          <button className="confirm-button" type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </div>
