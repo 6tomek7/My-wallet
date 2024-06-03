56ba011137fb015979f2a572239b1e24ef50f4e7
Tomasz Pasternak <tomasz.pasternak@euvic.pl>
2024-06-03 07:57:21 +0200

    changed form language to english

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import "./registration.css";
+
 const Registration = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({ username: null, password: null, confirmPassword: null });
 
   const validate = () => {
     const errors = {};
 
     if (username.length < 5) {
-      errors.username = "Nazwa użytkownika musi mieć minimum 5 znaków";
+      errors.username = "Username must be at least 5 characters long";
     }
 
     const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
     if (!passwordRegex.test(password)) {
-      errors.password = "Hasło musi mieć minimum 8 znaków, zawierać co najmniej jedną dużą literę i jeden znak specjalny";
+      errors.password = "Password must be at least 8 characters long, contain at least one uppercase letter and one special character";
     }
 
     if (password !== confirmPassword) {
-      errors.confirmPassword = "Hasła muszą się zgadzać";
+      errors.confirmPassword = "Passwords must match";
     }
 
     return errors;
@@ -36,26 +37,22 @@ const Registration = () => {
 
     const user = { username, email, password };
 
-    try {
-      const response = await fetch("http://test-database/user-registration", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
-      const data = await response.json();
-      console.log("Success:", data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    fetch("http://localhost:8080/test", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>
-          Nazwa użytkownika:
+          Username:
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
         </label>
         {errors.username && <p>{errors.username}</p>}
@@ -68,19 +65,19 @@ const Registration = () => {
       </div>
       <div>
         <label>
-          Hasło:
+          Password:
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </label>
         {errors.password && <p>{errors.password}</p>}
       </div>
       <div>
         <label>
-          Potwierdź hasło:
+          Confirm Password:
           <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
         </label>
         {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
       </div>
-      <button type="submit">Wyślij</button>
+      <button type="submit">Submit</button>
     </form>
   );
 };
